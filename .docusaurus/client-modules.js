5e6a1e377c45a1a354fe2d0b1adbcf4e16fbfbc0
Romain <romain@boutrois.com>
2023-11-23 14:37:13 +0100

    .gitignore is now working

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/romain/documentation/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/romain/documentation/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/romain/documentation/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/romain/documentation/src/css/custom.css"),
-];
